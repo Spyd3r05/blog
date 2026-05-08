f1ac4ec62d9db2a6d44744b65d96c3aca4e35a12
Spyd3r05 <agnesnzivu05@gmail.com>
2026-05-08 22:38:31 +0300

    feat(db): configure Prisma with PostgreSQL adapter and custom client generation

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,9 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+// /lib/prisma.ts
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../../src/generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
